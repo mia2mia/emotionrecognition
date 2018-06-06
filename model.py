2040bbbded216d5f46905b9f059b92abe2e6ee2a
Deep Chakraborty <deepc94@gmail.com>
2018-06-06 11:20:38 -0400

    Added Custom Mean Pooling Layer to support masking

--- model.py
+++ model.py
@@ -2,18 +2,49 @@ from __future__ import print_function
 from __future__ import division
 
 import numpy as np
+import tensorflow as tf
 
 import keras
-from keras.layers import Dense, Dropout, Masking, Average
+from keras.layers import Dense, Dropout, Masking
 from keras.layers import LSTM, TimeDistributed, Bidirectional
-from keras.layers import GlobalAveragePooling1D
+# from keras.layers import GlobalAveragePooling1D
 from keras.models import Sequential
+from keras import backend as K
+from keras.engine import InputSpec
+from keras.engine.topology import Layer
 
 from sklearn.utils import class_weight as clw
 from sklearn.utils import shuffle
 
 import time
 import os
+
+
+class MeanPool(Layer):
+    def __init__(self, **kwargs):
+        super(MeanPool, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.input_spec = InputSpec(ndim=3)
+
+    def compute_mask(self, input, input_mask=None):
+      # do not pass the mask to the next layers
+      return None
+
+    def call(self, x, mask=None):
+        if mask is not None:
+            # mask (batch, time)
+            mask = K.cast(mask, K.floatx())
+            # mask (batch, x_dim, time)
+            mask = K.repeat(mask, x.shape[-1])
+            # mask (batch, time, x_dim)
+            mask = tf.transpose(mask, [0,2,1])
+            x = x * mask
+        return K.sum(x, axis=1) / K.sum(mask, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        # remove temporal dimension
+        return (input_shape[0], input_shape[2])
+
 
 class emoLSTM():
     
@@ -23,8 +54,8 @@ class emoLSTM():
         self.model.add(TimeDistributed(Dense(512, activation='relu')))#, input_shape=(None, 40)))
         self.model.add(Dropout(0.5))
         self.model.add(Bidirectional(LSTM(128, return_sequences=True, dropout=0.5)))
-        #TODO: Fix the global average pooling layer to support masking
-        self.model.add(GlobalAveragePooling1D())
+        # self.model.add(GlobalAveragePooling1D())
+        self.model.add(MeanPool())
         # self.model.add(Average())
         self.model.add(Dense(6, activation='softmax'))
 
@@ -82,7 +113,7 @@ def bucket_generator(x_train, y_train, batch_size=32):
         while (counter+batch_size) < num_train:
             x_batch = pad_sequences(x_train[counter:counter+batch_size])
             y_batch = y_train[counter:counter+batch_size]
-            print (x_batch.shape, y_batch.shape)
+            # print (x_batch.shape, y_batch.shape)
             counter = counter + batch_size
             yield x_batch, y_batch
 
@@ -116,13 +147,14 @@ if __name__ == "__main__":
     class_weight = clw.compute_class_weight('balanced', np.unique(y_train), y_train)
     class_weight = dict(enumerate(class_weight))
     val_sample_weight = np.array([class_weight[cls] for cls in y_val])
-    val_sample_weight = val_sample_weight.reshape(-1,1)
+    # val_sample_weight = val_sample_weight.reshape(-1,1)
 
     # convert training labels to one hot vectors.
     y_train = keras.utils.to_categorical(y_train)
     y_val = keras.utils.to_categorical(y_val)
 
-    enn.fit(x_train[:100], y_train[:100], 
+    enn.fit(x_train[:1000], y_train[:1000], 
             batch_size=32, 
             epochs=2,
-            validation_data=(x_val[:10], y_val[:10], val_sample_weight[:10]), class_weight=class_weight)
+            validation_data=(x_val[:10], y_val[:10], val_sample_weight[:10]), 
+            class_weight=class_weight)
