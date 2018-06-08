9763ba42fe9f491a52414634f9a7d00845937f2f
Deep Chakraborty <deepc94@gmail.com>
2018-06-08 10:58:50 -0400

    Added intelligent bucket iterator, added dense layer and changed adam opt params

--- model.py
+++ model.py
@@ -8,16 +8,18 @@ import keras
 from keras.layers import Dense, Dropout, Masking
 from keras.layers import LSTM, TimeDistributed, Bidirectional
 # from keras.layers import GlobalAveragePooling1D
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras import backend as K
 from keras.engine import InputSpec
 from keras.engine.topology import Layer
+from keras import optimizers
 
 from sklearn.utils import class_weight as clw
 from sklearn.utils import shuffle
 
 import time
 import os
+from math import ceil
 
 
 class MeanPool(Layer):
@@ -26,9 +28,11 @@ class MeanPool(Layer):
         self.supports_masking = True
         self.input_spec = InputSpec(ndim=3)
 
+
     def compute_mask(self, input, input_mask=None):
       # do not pass the mask to the next layers
       return None
+
 
     def call(self, x, mask=None):
         if mask is not None:
@@ -41,6 +45,7 @@ class MeanPool(Layer):
             x = x * mask
         return K.sum(x, axis=1) / K.sum(mask, axis=1)
 
+
     def compute_output_shape(self, input_shape):
         # remove temporal dimension
         return (input_shape[0], input_shape[2])
@@ -48,33 +53,54 @@ class MeanPool(Layer):
 
 class emoLSTM():
     
-    def __init__(self):
+    def __init__(self, pre_trained=None):
+        if pre_trained is None:
+            self.build_model()
+        else:
+            self.model = load_model(pre_trained)
+        
+
+    def build_model(self):
         self.model = Sequential()
+        # Masking layer to ignore the 0 padded values
         self.model.add(Masking(mask_value=0.0, input_shape=(None, 40)))
+
+        # Dense layers for LLD learning
         self.model.add(TimeDistributed(Dense(512, activation='relu')))#, input_shape=(None, 40)))
         self.model.add(Dropout(0.5))
+        self.model.add(TimeDistributed(Dense(512, activation='relu')))
+        self.model.add(Dropout(0.5))
+
+        # Bidirectional LSTM Layer
         self.model.add(Bidirectional(LSTM(128, return_sequences=True, dropout=0.5)))
+
+        # Average Pooling Layer for combining all time steps' outputs
         # self.model.add(GlobalAveragePooling1D())
         self.model.add(MeanPool())
         # self.model.add(Average())
+        
+        # Final Classification Layer with Softmax activation
         self.model.add(Dense(6, activation='softmax'))
 
+        # Loss Function and Metrics
+        adam = optimizers.Adam(lr=0.01)
         self.model.compile(loss='categorical_crossentropy', 
-                            optimizer='adam', 
+                            optimizer=adam, 
                             metrics=['accuracy'],
                             weighted_metrics=['accuracy'])
-        
+
+
     def fit(self, x_train, y_train,
             batch_size=32, 
             epochs=25,
             validation_data=None, class_weight=None):
         
         x_val, y_val, val_sample_weight = validation_data
-        steps_per_epoch = len(x_train) // batch_size
+        steps_per_epoch = ceil(len(x_train) / batch_size)
         # validation_steps = len(x_val) #// batch_size
         # number_of_batches = len(x_train)
         self.model.fit_generator(
-                        bucket_generator(x_train, y_train, batch_size=batch_size), 
+                        generator=intel_bucket_generator(x_train, y_train, batch_size=batch_size), 
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs, 
                         class_weight=class_weight,
@@ -99,23 +125,38 @@ class emoLSTM():
         return self.model.predict(x_test)
 
 
-# def data_generator(x_train, y_train):
+# def bucket_generator(x_train, y_train, batch_size=32):
+#     num_train = len(x_train)
 #     while True:
+#         counter = 0
 #         x_train, y_train = shuffle(x_train, y_train)
-#         for x,y in zip(x_train, y_train):
-#             yield x[None,:], y[None,:]
+#         while counter < num_train:
+#             x_batch = pad_sequences(x_train[counter:counter+batch_size])
+#             y_batch = y_train[counter:counter+batch_size]
+#             # print (x_batch.shape, y_batch.shape)
+#             counter = counter + batch_size
+#             yield x_batch, y_batch
 
-def bucket_generator(x_train, y_train, batch_size=32):
+
+def intel_bucket_generator(x_train, y_train, batch_size=64):
     num_train = len(x_train)
+    seq_lens = [example.shape[0] for example in x_train]
+    sort_indices = np.argsort(seq_lens)
+    x_train = x_train[sort_indices]
+    y_train = y_train[sort_indices]
+    #check if it worked
+    print (x_train[0].shape[0], x_train[-1].shape[0])
+
     while True:
         counter = 0
-        x_train, y_train = shuffle(x_train, y_train)
-        while (counter+batch_size) < num_train:
+        while counter < num_train:
             x_batch = pad_sequences(x_train[counter:counter+batch_size])
             y_batch = y_train[counter:counter+batch_size]
-            # print (x_batch.shape, y_batch.shape)
+            x_batch, y_batch = shuffle(x_batch, y_batch)
+            print (x_batch.shape[1])
             counter = counter + batch_size
             yield x_batch, y_batch
+
 
 def pad_sequences(mini_batch):
     batch = np.copy(mini_batch)
@@ -126,13 +167,6 @@ def pad_sequences(mini_batch):
         if seq_len != max_len:
             batch[i] = np.vstack([example, np.zeros((max_len - seq_len, 40), dtype=example.dtype)])
     return np.dstack(batch).transpose(2,0,1)
-
-# def val_generator(x_val, y_val, val_sample_weight):
-#     while True:
-#         x_val, y_val, val_sample_weight = shuffle(x_val, y_val, val_sample_weight)
-#         for x,y,w in zip(x_val, y_val, val_sample_weight):
-#             # print (x[None,:].shape, y[None,:].shape, w.shape)
-#             yield x[None,:], y[None,:], w
 
 
 if __name__ == "__main__":
@@ -154,7 +188,24 @@ if __name__ == "__main__":
     y_val = keras.utils.to_categorical(y_val)
 
     enn.fit(x_train[:1000], y_train[:1000], 
-            batch_size=32, 
+            batch_size=64, 
             epochs=2,
             validation_data=(x_val[:10], y_val[:10], val_sample_weight[:10]), 
             class_weight=class_weight)
+
+
+"""deprecated batch size 1 data generator
+# def data_generator(x_train, y_train):
+#     while True:
+#         x_train, y_train = shuffle(x_train, y_train)
+#         for x,y in zip(x_train, y_train):
+#             yield x[None,:], y[None,:]
+
+
+# def val_generator(x_val, y_val, val_sample_weight):
+#     while True:
+#         x_val, y_val, val_sample_weight = shuffle(x_val, y_val, val_sample_weight)
+#         for x,y,w in zip(x_val, y_val, val_sample_weight):
+#             # print (x[None,:].shape, y[None,:].shape, w.shape)
+#             yield x[None,:], y[None,:], w
+"""
